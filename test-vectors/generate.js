d5dd1eb48056f099095dee7aab0a67bc440d487b
Orie Steele <orie@transmute.industries>
2022-11-29 17:30:15 -0600

    Add example

---
+++ test-vectors/generate.js
@@ -0,0 +1,27 @@
+const jose = require('jose');
+const fs = require('fs');
+
+const credential = require('../credential.json');
+const payload = new TextEncoder().encode(JSON.stringify(credential));
+
+const privateKeyJwk = {
+  "kid": "urn:ietf:params:oauth:jwk-thumbprint:sha-256:GNcrpR6vVHCzTe7W-9ntbJx_5hHfhiVCHWHEUaC4E_c",
+  "kty": "EC",
+  "crv": "P-256",
+  "alg": "ES256",
+  "x": "51Q_KAFsFu_FOjPojMN_Nt_pWmwokjn7iF6p3LiUiuk",
+  "y": "T58_jpJ03yFV3D13hACKiZpjRwXl92XZEv4Lt_OkUK4",
+  "d": "bAv9YrE443mH36LnzixS3Kv5ThM5m9mH7Ab_BiU3JZc"
+};
+
+
+(async ()=>{
+
+  const privateKey = await jose.importJWK(privateKeyJwk);
+  const jws = await new jose.CompactSign(payload)
+      .setProtectedHeader({ kid: credential.issuer + '#key-0', alg: privateKeyJwk.alg, })
+      .sign(privateKey);
+
+  fs.writeFileSync('../verifiable-credential.jws', jws);
+
+})()
